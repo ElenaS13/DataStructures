aadf75fa22024f2c2c2930bb88ddf58d15bffc57
Elena Sidyakina <elena.sidyakina@nfl.com>
2020-05-11 06:51:29 -0700

    recursive binary search practice

---
+++ recursiveSearchPractice.java
@@ -0,0 +1,26 @@
+
+public class recursiveSearchPractice {
+
+	public static void main(String[] args) {
+		System.out.println(recursiveBinarySearch(new int [] {1,2,3,4,5,67,90,99}, 0, 7, 99));
+
+	}
+	
+	public static int recursiveBinarySearch(int [] a, int p, int r, int x ) {
+		if(p > r) {
+			return -1; 
+		} else {
+			int q = (p+r)/2;
+			if(a[q] == x) {
+				return q;
+			} else if(a[q] > x) {
+				return recursiveBinarySearch(a, p, q-1, x);
+			} else {
+				return recursiveBinarySearch(a, q+1, r, x);
+			}
+		}
+		 
+	}
+	
+
+}
