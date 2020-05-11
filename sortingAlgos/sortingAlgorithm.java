2abe629976ed04ff4b103e15b10f924abcadc811
Elena Sidyakina <elena.sidyakina@nfl.com>
2020-05-11 10:54:37 -0700

    added package for sorting algos and one algo

---
+++ sortingAlgos/sortingAlgorithm.java
@@ -0,0 +1,33 @@
+package sortingAlgos;
+
+public class sortingAlgorithm {
+
+	public static void main(String[] args) {
+		int [] myArray = selectionSort(new int[] {9,4,2,98,5,46});
+		for(int i =0; i < myArray.length; i++) {
+		System.out.println(myArray[i]);
+		
+		
+		}
+
+	}
+
+	public static int [] selectionSort(int a []) {
+		for(int i =0; i < a.length; i++) {
+			int min = i; 
+			for (int j= i+1; j < a.length; j++) {
+				if(a[j] < a[min]) {
+					min = j;
+				}
+			}
+			
+			int temp = a[i]; 
+			a[i] = a[min];
+			a[min] = temp; 
+			
+		}
+		
+		return a; 
+		
+	}
+}
