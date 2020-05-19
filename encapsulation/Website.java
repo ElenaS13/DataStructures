a5a877cc507ec1c2d9930283cc2ab0306663794b
Elena Sidyakina <elena.sidyakina@nfl.com>
2020-05-19 10:32:17 -0700

    OOP practice

---
+++ encapsulation/Website.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class Website {
+	   //fields (or instance variable)
+	   String webName;
+	   int webAge;
+
+	   // constructor
+	   Website(String name, int age){
+	      this.webName = name;
+	      this.webAge = age;
+	   }
+}
