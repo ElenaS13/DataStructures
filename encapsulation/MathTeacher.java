338afb57b2d073e466f485a7fd7037591e3166bd
Elena Sidyakina <elena.sidyakina@nfl.com>
2020-05-19 10:58:09 -0700

    practicing Inheritance

---
+++ encapsulation/MathTeacher.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class MathTeacher extends Teacher {
+	
+	String mainSubject = "Maths";
+
+	public static void main(String[] args) {
+		MathTeacher obj = new MathTeacher();
+	      System.out.println(obj.college);
+	      System.out.println(obj.designation);
+	      System.out.println(obj.mainSubject);
+	      obj.does();
+
+	}
+
+}
---
+++ encapsulation/Teacher.java
@@ -0,0 +1,9 @@
+package encapsulation;
+
+public class Teacher {
+	   String designation = "Teacher";
+	   String college = "Beginnersbook";
+	   void does(){
+		System.out.println("Teaching");
+	   }
+	}
