d095a8a3f88abb56798f2c7aa0cb1fabbae788ca
Elena Sidyakina <elena.sidyakina@nfl.com>
2020-05-25 15:05:24 -0700

    practicing singly linked lists

---
+++ linkedlistPractice/App1.java
@@ -0,0 +1,23 @@
+package linkedlistPractice;
+
+public class App1 {
+
+	public static void main(String[] args) {
+		
+		SinglyLinkedList1 sList = new SinglyLinkedList1();
+		
+		// add nodes to the list 
+		
+		sList.addNode(1);
+		sList.addNode(2);
+		sList.addNode(3);
+		sList.addNode(4);
+		
+		
+		// display the nodes present in the list 
+		
+		sList.display();
+
+	}
+
+}
---
+++ linkedlistPractice/Node1.java
@@ -0,0 +1,14 @@
+package linkedlistPractice;
+
+public class Node1 {
+	
+	//represent a node of the singly linked list
+	int data;
+	Node next;
+	
+	public Node1(int data) {
+		this.data = data;
+		this.next = null;
+	}
+
+}
